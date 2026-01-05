fd2721c5e93f98be60323a833268f9dfc7e95a16
Edward46282 <eoedward7353@gmail.com>
2026-01-05 03:16:39 -0500

    Add education section

--- MyIntro/MyIntro/src/App.jsx
+++ MyIntro/MyIntro/src/App.jsx
@@ -1,5 +1,14 @@
 import myVideo from './assets/editedVideo.mp4'
+import uniFlag from './assets/uottawa-logo.png'
+import uniCampus from './assets/uottawa-campus.jpg'
+import uniStem from './assets/uottawa-STEM.jpg'
 import './App.css'
+import { useState, useEffect } from 'react';
+
+const images = [
+  uniCampus, uniFlag, uniStem
+];
+
 
 function App() {
   return (
@@ -9,8 +18,9 @@ function App() {
         <div className="split-view">
           {/* Left Side: Blue Box */}
           <div className="left-half">
-            <div className="blue-box">
+            <div className="black-box">
               <h1>Edward Eo</h1>
+              <div className="lineTag"></div>
               <p>info to come</p>
             </div>
           </div>
@@ -30,6 +40,21 @@ function App() {
           </div>
         </div>
         <div className="gradient-spacer"></div>
+        <div className="education-container">
+          <h1 style={{ fontStyle: 'italic' }}>&emsp;&emsp;Education</h1>
+          <div className= "education-content">
+            <div className = "uniPhoto">
+              <ImageCard />
+            </div>
+            <div className = "uniDescription">
+              <h3>Computer Science Student At</h3>
+              <h2> University of Ottawa</h2>
+              <div className="description-linespacer"></div>
+              <p>Description coming</p>
+            </div>
+          </div>
+        </div>
+        
       </div>
 
       {/* 2. The Right-Side Navbar */}
@@ -43,6 +68,37 @@ function App() {
       </nav>
     </div>
   )
+}
+
+function ImageCard() {
+  const [index, setIndex] = useState(0);
+
+  // Arrow function to go to next image
+  const nextImage = () => {
+    setIndex((prev) => (prev + 1) % images.length); // no-name function that takes prev index as a parameter
+  };
+
+  // Timer: Change every 15 seconds
+  useEffect(() => {
+    const timer = setInterval(nextImage, 15000);
+    return () => clearInterval(timer); // Cleanup on unmount
+  }, [index]);
+
+  const nextIndex = (index + 1) % images.length;
+
+  return (
+    <div className="carousel-container">
+      <div className="card-stack">
+        {/* Main Image */}
+        <img src={images[index]} key={index} className="main-card" alt="Active" />
+        
+        {/* Upcoming Tilted Image */}
+        <img src={images[nextIndex]} key={nextIndex} className="next-card" alt="Next" />
+      </div>
+      
+      <button onClick={nextImage} className="nav-button">Next Image</button>
+    </div>
+  );
 }
 
 export default App
